a93d92e642ad47f2896f8f6784c5adaec7f5c6fc
Glo03 <110479000+Glo03@users.noreply.github.com>
2024-02-23 10:05:05 -0500

    all wed webdesign created with images

---
+++ script1.js
@@ -0,0 +1,4 @@
+document.getElementById("clothing-type").addEventListener("change", function() {
+    var selectedOption = this.value;
+    // You can add your logic here based on the selected option
+});
